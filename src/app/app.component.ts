c860d9e12a951f87daf6f4e04473029e314d3c8c
Alexander Matveev <sa-matik@yandex.com>
2023-07-04 01:25:15 +0300

    Make edit/delete buttons work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Source} from "./source";
+import {EditModal} from "./edit-modal";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ export class AppComponent  {
   activeSource: Source = new Source();
   words: any[] = [];
   serverUrl = 'http://178.209.46.147:8080/slovaro';
+  modalData =  new EditModal();
 
   constructor(private http: HttpClient) {
     this.http = http;
@@ -82,5 +84,111 @@ export class AppComponent  {
         (error) => console.log(error)
         );
 
+    }
+
+    showModal(mode: string, event: any){
+        console.log('tight click');
+        let target = event.target as HTMLElement;
+        let modal;
+
+        modal = document.querySelectorAll('#edit-modal')[0] as HTMLElement;
+
+
+        this.modalData.setElement(target);
+        this.modalData.setType(mode);
+
+          if(modal){
+                modal.classList.remove('hidden');
+
+            let rect = target.getBoundingClientRect();
+
+            modal.style.position='absolute';
+            modal.style.left = rect.left + 'px';
+            modal.style.top = rect.top + 'px';
+
+              let inputField = document.querySelectorAll('#edit-modal input')[0] as HTMLInputElement;
+              inputField.style.width = target.offsetWidth + 'px';
+              mode === 'source'? modal.classList.add('modal-for-source') : modal.classList.remove('modal-for-source');
+
+              inputField.value = target.innerHTML;
+              document.querySelectorAll('.modal-root')[0].classList.add('visible');
+
+        }
+    }
+
+    closeModal(){
+        document.querySelectorAll('.modal-root')[0].classList.remove('visible');
+        document.querySelectorAll('#edit-modal')[0].classList.toggle('hidden');
+      }
+
+    editEntry(){
+
+    if (this.modalData.getType() === 'source'){
+      let entry = (document.querySelectorAll('#edit-modal input')[0] as HTMLInputElement).value;
+
+      const body = {
+        userId: 1,
+        name: entry,
+        id: this.modalData.getElement().getAttribute('data-sourceid')
+        };
+       this.http.put(this.serverUrl + "/sources", body).subscribe(
+        (data: any) => {
+            console.log(data);
+            this.getSources();
+          },
+        (error) => console.log(error)
+        );
+
+      }
+
+    if (this.modalData.getType() === 'word'){
+      let entry = (document.querySelectorAll('#edit-modal input')[0] as HTMLInputElement).value;
+
+      const body = {
+        userId: 1,
+        content: entry,
+        id: this.modalData.getElement().getAttribute('data-wordid'),
+        sourceId: this.activeSource.getId()
+        };
+       this.http.put(this.serverUrl + "/words", body).subscribe(
+        (data: any) => {
+            console.log(data);
+            this.getSources();
+          },
+        (error) => console.log(error)
+        );
+      }
+    }
+
+    deleteEntry(){
+
+    if (this.modalData.getType() === 'source'){
+      let sourceId = this.modalData.getElement().getAttribute('data-sourceid');
+
+    this.http.delete(this.serverUrl + "/sources/" + sourceId).subscribe((data: any) =>
+      {
+      console.log(data);
+      this.getSources();
+      }
+    );
+      this.closeModal();
+
+      }
+
+    if (this.modalData.getType() === 'word'){
+      let wordId = this.modalData.getElement().getAttribute('data-wordid');
+
+    this.http.delete(this.serverUrl + "/words/" + wordId).subscribe((data: any) =>
+      {
+      console.log(data);
+      this.getWords(this.activeSource.getId());
+      }
+    );
+      this.closeModal();
+
+      }
+
+
+      return;
     }
 }
